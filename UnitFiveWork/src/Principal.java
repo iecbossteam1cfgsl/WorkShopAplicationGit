e9835567a2a773094654d8cdb33db1dd12b67be8
iecbossteam1cfgsl <bossteam1cfgsl@gmail.com>
2018-03-21 20:34:29 +0100

    Add some refactors

--- UnitFiveWork/src/Principal.java
+++ UnitFiveWork/src/Principal.java
@@ -2,8 +2,29 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		showWellcome();
 
+		doOperationAdd();
+	}
+
+	/**
+	 * @return TODO
+	 * 
+	 */
+	private static int doOperationAdd() {
+		int a=1;
+		int b=1;
+		return a+b;
+		//System.out.println("El resultado de la suma es"+result);
+	}
+
+	/**
+	 * 
+	 */
+	private static void showWellcome() {
+		System.out.println("*************************************");
+		System.out.println("Bienvenido al supermercado El Caminas");
+		System.out.println("*************************************");
 	}
 
 }
