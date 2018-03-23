2b42078f86c9af8d21bea352e88fc9acdcc68f09
iecbossteam1cfgsl <iecbossteam1cfgsl@gmail.com>
2018-03-23 14:53:18 +0100

    refactor main menu

--- UnitFiveWork/src/Principal.java
+++ UnitFiveWork/src/Principal.java
@@ -2,16 +2,20 @@
 public class Principal {
 
 	public static void main(String[] args) {
+		mainMenu();
+		
+		
+
+		
+	}
+
+	private static void mainMenu() {
 		System.out.println("**********************Menu ****************");
 		System.out.println("Dar de alta cliente------>1");
 		System.out.println("Dar de alta producto----->2");
 		System.out.println("Linea de factura--------->3");
 		System.out.println("Calcular importe--------->4");
 		System.out.println("Salir-------------------->5");
-		
-		
-
-		
 	}
 
 	
